d4b2ba8b9de6862c15224b8bec0e0d2c967495a5
sam4om <semenskurihin@gmail.com>
2020-02-15 14:00:34 +0300

    Первичная загрузка строки

--- pars.py
+++ pars.py
@@ -13,13 +13,37 @@ def url_pull():
 
 #Функция обхода страниц, начало с базового урла
 def parse(base_url, headers):
+    table_string =[]
     session = requests.Session() #Эмуляция сессии
     request = session.get(base_url, headers=headers)
     if request.status_code == 200:
         soup = bs(request.content, 'html.parser')
-        table = soup.find("table").text
-        print(table)
-        while '\t' in table:
+        table = soup.find("table").find_all("tr")
+
+        stringintable = table[5]
+        for td in stringintable:
+            rn = stringintable.find_all("td")[0].get_text().strip()
+            sr = stringintable.find_all("td")[1].get_text().strip()
+            d = stringintable.find_all("td")[2].get_text().strip()
+            n = stringintable.find_all("td")[3].get_text().strip()
+
+
+        data = {
+            'number': rn,
+            'adress': sr,
+            'PM10': d,
+            'PM25':n
+        }
+        print(data)
+
+
+
+        #for i in range(len(table)):
+        #    if table[i] != '' and table[i] != '\n':
+                # table_string.append(table[i])
+        #Stringtable = ''.join(table_string)
+        #print(Stringtable)
+
 
 
 
