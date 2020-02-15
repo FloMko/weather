c45de6e90bfc9c241a5a0b6e0a688a540b7dd7e5
sam4om <semenskurihin@gmail.com>
2020-02-15 16:30:42 +0300

    fix

--- pars.py
+++ pars.py
@@ -4,15 +4,12 @@ from bs4 import BeautifulSoup as bs
 import influx
 import logging
 
-logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.WARN)
 
 
 headers = {"accept": "*/*",
            "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36'}
 #Начало парсинга со страницы арентдных квартир в Санкт-Петербурге
-
-
-base_url = "http://www.infoeco.ru/index.php?id=2276"
 
 
 
@@ -40,37 +37,39 @@ def urls_pull(linklist = list):
     return linklist
 
 #Функция обхода страниц, начало с базового урла
-def parse(base_url, headers):
+def parse(link, headers):
     table_string =[]
+    unpadsed_url = []
     session = requests.Session() #Эмуляция сессии
-    request = session.get(base_url, headers=headers)
+    request = session.get(link, headers=headers)
     if request.status_code == 200:
-        soup = bs(request.content, 'html.parser')
-        table = soup.find("table").find_all("tr")
-        rows = soup.find("table", border=1).find("tbody").find_all("tr")
-        date = soup.find("table", border=1).find("td").get_text().strip().split(' ')[-1]
-        for i in range(len(rows)):
-            try:
-                stringintable = table[i]
-                for td in stringintable:
-                    rn = stringintable.find_all("td")[0].get_text().strip()
-                    sr = stringintable.find_all("td")[1].get_text().strip()
-                    d = stringintable.find_all("td")[2].get_text().strip()
-                    n = stringintable.find_all("td")[3].get_text().strip()
+        try:
+            soup = bs(request.content, 'html.parser')
+            table = soup.find("table").find_all("tr")
+            rows = soup.find("table", border=1).find("tbody").find_all("tr")
+            date = soup.find("table", border=1).find("td").get_text().strip().split(' ')[-1]
+            for i in range(len(rows)):
+                try:
+                    stringintable = table[i]
+                    for td in stringintable:
+                        rn = stringintable.find_all("td")[0].get_text().strip()
+                        sr = stringintable.find_all("td")[1].get_text().strip()
+                        d = stringintable.find_all("td")[2].get_text().strip()
+                        n = stringintable.find_all("td")[3].get_text().strip()
 
-                data = {
-                    'date': date,
-                    'url': base_url,
-                    'index': rn,
-                    'address': sr,
-                    'pm10': d,
-                    'pm2_5':n
-                }
-                influx.populate(data['date'],data['index'],data['url'], data['address'], data['pm10'], data['pm2_5'])
-                print(data)
-            except:
-                pass
-
+                    data = {
+                        'date': date,
+                        'url': link,
+                        'index': rn,
+                        'address': sr,
+                        'pm10': d,
+                        'pm2_5':n
+                    }
+                    influx.populate(data['date'],data['index'],data['url'], data['address'], data['pm10'], data['pm2_5'])
+                except:
+                    pass
+        except:
+            logging.error(f'{link}')
 
 linklist = urls_pull()
 for link in linklist:
