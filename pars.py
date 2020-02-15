5bad6c65b78e9c1fd5075e3d9861baf0ffcd56b9
sam4om <semenskurihin@gmail.com>
2020-02-15 16:20:47 +0300

    Added urls iterations, but some part of that code need to fix

--- pars.py
+++ pars.py
@@ -2,15 +2,42 @@ import csv
 import requests
 from bs4 import BeautifulSoup as bs
 import influx
+import logging
+
+logging.getLogger().setLevel(logging.DEBUG)
 
 
 headers = {"accept": "*/*",
            "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36'}
 #Начало парсинга со страницы арентдных квартир в Санкт-Петербурге
+
+
 base_url = "http://www.infoeco.ru/index.php?id=2276"
 
-def url_pull():
-    pass
+
+
+
+
+def urls_pull(linklist = list):
+    linklist = []
+    urlsforears = ['http://www.infoeco.ru/index.php?id=2820', 'http://www.infoeco.ru/index.php?id=3122', 'http://www.infoeco.ru/index.php?id=4355', 'http://www.infoeco.ru/index.php?id=6532', 'http://www.infoeco.ru/index.php?id=8221']
+    for url_year in urlsforears:
+        for index in range(500):
+            if index % 20 == 0 or index - 1 == 0:
+                url = url_year + f'&start={index}'
+                session = requests.Session()  # Эмуляция сессии
+                request = session.get(url, headers=headers)
+                if request.status_code == 200:
+                    soup = bs(request.content, 'html.parser')
+                    content = soup.find_all(attrs={'class': 'clearfix'})
+                    for i in content:
+                        partoflink = i.find('a').attrs['href']
+                        link = "http://www.infoeco.ru/" + partoflink
+                        linklist.append(link)
+            else:
+                pass
+
+    return linklist
 
 #Функция обхода страниц, начало с базового урла
 def parse(base_url, headers):
@@ -45,5 +72,9 @@ def parse(base_url, headers):
                 pass
 
 
+linklist = urls_pull()
+for link in linklist:
+    parse(link,headers)
+    logging.debug(link)
 
-parse(base_url, headers)
+#parse(base_url, headers)
